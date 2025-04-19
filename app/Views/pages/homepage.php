c4f75f89ff9956b421960b33a734281c05f2c6d8
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-04-19 13:06:51 +0800

    Update routing configuration to enable auto routing and change stylesheet link to minified version

---
+++ app/Views/pages/homepage.php
@@ -0,0 +1,10 @@
+<?= $this->extend('layouts/wrapper') ?>
+
+<?= $this->section('content') ?>
+<div class="row">
+    <div class="col-12">
+        <h1>Selamat Datang di E-Commerce Hexadigital</h1>
+        <p>Ini adalah halaman utama website kami.</p>
+    </div>
+</div>
+<?= $this->endSection() ?>
